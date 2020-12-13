cf19842b2dd3c9a2f1a3fec7a3a4dfb4caea6c2f
sami <dcil142@samiti.speedport.ip>
2020-12-13 14:19:35 +0100

    datepicker selected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
 import './App.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <DatePicker
+        selected={selectedDate}
+        onChange={(date) => setSelectedDate(date)}
+      />
     </div>
   );
 }
